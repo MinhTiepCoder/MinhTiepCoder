4bfc27f3d9258f5a1554352ec3e8f8e50b05951f
MinhTiepCoder <89243797+MinhTiepCoder@users.noreply.github.com>
2022-07-18 13:33:59 +0700

    SYDO CORP

--- dist/html/assets/js/theme.js
+++ dist/html/assets/js/theme.js
@@ -1020,3 +1020,12 @@
     }
   })();
 })(jQuery);
+$("#back-to-top").click(function(){return $("body, html").animate({scrollTop:0},400),!1});
+$(function(){$('[data-toggle="tooltip"]').tooltip()});
+
+$("figure").mouseleave(
+  function() {
+    $(this).removeClass("hover");
+  }
+);
+
